4c4d302cd559d0fcbace72ddbbad97689cd57c71
Danilo Alves <danilovieiraalves@hotmail.com>
2022-12-04 00:20:53 -0300

    feat(core): adds tabs management keys

--- lua/alves/core/keymaps.lua
+++ lua/alves/core/keymaps.lua
@@ -2,7 +2,7 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
--- Windows
+-- Windows management
 keymap.set("n", "<leader>wv", "<C-w>v") -- split window vertically
 keymap.set("n", "<leader>ws", "<C-w>s") -- split window horizontally
 keymap.set("n", "<leader>we", "<C-w>=") -- make split windows equal width
@@ -12,6 +12,12 @@ keymap.set("n", "<leader>wh", ":TmuxNavigateLeft<CR>") -- move to left window
 keymap.set("n", "<leader>wj", ":TmuxNavigateDown<CR>") -- move to bottom window
 keymap.set("n", "<leader>wd", ":close<CR>") -- close current split window
 keymap.set("n", "<leader>wm", ":MaximizerToggle<CR>") -- maximizes or restore current window
+
+-- Tabs management
+keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
+keymap.set("n", "<leader>td", ":tabclose<CR>") -- close current tab
+keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
+keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
 
 -- File explorer
 keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") -- maximizes or restore current window
